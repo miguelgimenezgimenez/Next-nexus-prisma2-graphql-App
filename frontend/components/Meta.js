3b1379d6cb01a35b0211a0c2ff7b9b52f36f24c3
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-06 17:24:23 +0100

    feat frontend initial configuration

---
+++ frontend/components/Meta.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <link rel="shortcut icon" href="https://images.ctfassets.net/5gv1edeicqfs/48EM0LU3Z6gWkQCcCaeoq2/704ea273b5d50d09ff450a5ceaa74631/guidesmiths-logo.png" />
+    <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+    <title>Guide Smiths</title>
+  </Head>
+);
+
+export default Meta;
---
+++ frontend/components/Page/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { ThemeProvider } from 'styled-components';
+import Header from './Header';
+import Meta from './Meta';
+import { theme, StyledPage, Inner } from './styles'
+class Page extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default Page;
