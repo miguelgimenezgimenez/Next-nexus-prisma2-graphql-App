60c2d2355a1759f6da1e880af8e0049a521fff2b
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-08 18:00:32 +0100

    fix page component

--- frontend/components/Page/index.js
+++ frontend/components/Page/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { ThemeProvider } from 'styled-components';
-import Header from './Header';
-import Meta from './Meta';
+import Header from '../Header';
+import Meta from '../Meta';
 import { theme, StyledPage, Inner } from './styles'
 class Page extends Component {
   render() {
