f456b1f3564621fdc088b16d3660c16a7c98b017
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-08 11:13:45 +0100

    docker dump db

--- backend_graphQL/database/dump.sql
+++ backend_graphQL/database/dump.sql
@@ -1,14 +1,14 @@
 create table  Device(
    id  SERIAL PRIMARY KEY,
-   brandName  VARCHAR(100)    NOT NULL,
+   brand  VARCHAR(100)    NOT NULL,
    link  VARCHAR(100),
    image  VARCHAR(100),
    name      VARCHAR(100)    NOT NULL,
    dimensions  VARCHAR(100),
    os        VARCHAR(100),
-   storage        VARCHAR(100),
+   storage        VARCHAR(100)
 );
 
 
-copy Device (brandName,link,image,name,dimensions,os,storage
-) from './acer-phones_export.csv'  DELIMITER ',' CSV HEADER; 
+copy Device (brand,link,image,name,dimensions,os,storage
+) from '/fixtures/all_brands_export.csv'  DELIMITER ';' CSV HEADER; 
