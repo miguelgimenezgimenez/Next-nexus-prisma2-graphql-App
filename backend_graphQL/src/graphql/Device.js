4156a87aca2575c0b220bcfe495c55156043c643
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-08 11:17:42 +0100

    clean up

--- backend_graphQL/src/graphql/Device.js
+++ backend_graphQL/src/graphql/Device.js
@@ -1,16 +1,5 @@
 const { objectType, extendType, stringArg, intArg } = require('@nexus/schema')
 
-
-
-// export const Post = objectType({
-//   name: 'Post',            // <- Name of your type
-//   definition(t) {
-//     t.int('id')            // <- Field named `id` of type `Int`
-//     t.string('title')      // <- Field named `title` of type `String`
-//     t.string('body')       // <- Field named `body` of type `String`
-//     t.boolean('published') // <- Field named `published` of type `Boolean`
-//   },
-// })
 const Device = objectType({
   name: 'Device',
   definition(t) {
