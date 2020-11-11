070a9f7856c869761077e3a5bfb0b39a9ad4071d
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-11 16:41:33 +0100

    fix delete item

--- frontend/components/DeleteItem/index.js
+++ frontend/components/DeleteItem/index.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
+import { PHONE_CONNECTION_QUERY } from '../PhonesList';
 
-const DELETE_ITEM_MUTATION = gql`
-  mutation DELETE_ITEM_MUTATION($id: ID!) {
-    deleteItem(id: $id) {
+const DELETE_PHONE_MUTATION = gql`
+  mutation DELETE_PHONE_MUTATION($id: Int!) {
+    deletePhone(id: $id) {
       id
     }
   }
 `;
 
 function update(cache, payload) {
-  cache.evict(cache.identify(payload.data.deleteItem));
+  cache.evict(cache.identify(payload.data.deletePhone));
 }
 
 function DeleteItem({ id, children }) {
-  const [deleteItem] = useMutation(DELETE_ITEM_MUTATION, {
+  const [deleteItem] = useMutation(DELETE_PHONE_MUTATION, {
     variables: { id },
     update,
-
   });
   return (
     <button
       type="button"
       onClick={() => {
         if (confirm('Are you sure you want to delete this item?')) {
-          deleteItem().catch(err => {
-            alert(err.message);
-          });
+          deleteItem()
+            .catch(err => {
+              alert(err.message);
+            });
         }
       }}
     >
